7c74db0d62dcc4707504392fe50da7d128e9e4b0
tobif <saibot01141@gmail.com>
2023-08-12 00:28:36 +0200

    small updates

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import selenium
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import ElementClickInterceptedException
+
 import time
 
 SIMILAR_ACCOUNT = "elonrmuskk"
@@ -18,7 +20,6 @@ class InstaFollower:
         self.driver = selenium.webdriver.Chrome(options=OPTIONS)
         self.driver.maximize_window()
 
-
     def login(self):
         self.driver.get("https://instagram.com")
         time.sleep(3)
@@ -30,40 +31,25 @@ class InstaFollower:
         time.sleep(2)
         self.driver.find_element(By.ID, 'email').send_keys(USERNAME)
         self.driver.find_element(By.ID, 'pass').send_keys(PASSWORD)
-
-
-    # def follow(self):
-    # 
-    #     print("I I will follow you")
-
-
-    # def find_follower(self):
-    #     # - just for now
-    #     self.driver.get("https://www.instagram.com/elonrmuskk/")
-    #     time.sleep(3)
-    #     self.driver.find_element(By.XPATH, '//button[contains(text(), "Decline optional cookies")]').click()
-    #     time.sleep(3)
-    #
-    #     search_box = self.driver.find_element(By.XPATH, '//input[@placeholder="Search"]')
-    #     search_box.send_keys(SIMILAR_ACCOUNT)
-    #     search_box.send_keys(Keys.ENTER)
+        self.driver.find_element(By.ID, 'pass').send_keys(Keys.ENTER)
 
     def find_followers(self):
         time.sleep(5)
         self.driver.get(f"https://www.instagram.com/{SIMILAR_ACCOUNT}")
 
         time.sleep(2)
-        followers = self.driver.find_element(By.XPATH,
-            '//*[@id="react-root"]/section/main/div/header/section/ul/li[2]/a')
+        followers = self.driver.find_element(By.XPATH, '//button[contains(text(), "followers")]')
         followers.click()
 
         time.sleep(2)
-        modal = self.driver.find_element(By.XPATH, '/html/body/div[4]/div/div/div[2]')
-        for i in range(10):
+        # modal = self.driver.find_element(By.XPATH, '/html/body/div[4]/div/div/div[2]')
+        modal = self.driver.find_element(By.XPATH, '//span[contains(text(), "Meta")]')
+        for i in range(1):
             # In this case we're executing some Javascript, that's what the execute_script() method does.
-            # The method can accept the script as well as a HTML element.
+            # The method can accept the script as well as an HTML element.
             # The modal in this case, becomes the arguments[0] in the script.
-            # Then we're using Javascript to say: "scroll the top of the modal (popup) element by the height of the modal (popup)"
+            # Then we're using Javascript to say: "scroll the top of the modal (popup)
+            # element by the height of the modal (popup)"
             self.driver.execute_script("arguments[0].scrollTop = arguments[0].scrollHeight", modal)
             time.sleep(2)
 
@@ -76,12 +62,13 @@ class InstaFollower:
             except ElementClickInterceptedException:
                 cancel_button = self.driver.find_element('/html/body/div[5]/div/div/div/div[3]/button[2]')
                 cancel_button.click()
-                
-                
+
+
 
 insta_bot = InstaFollower()
 
+insta_bot.login()
 
-# insta_bot.login()
+insta_bot.find_followers()
+
 insta_bot.follow()
-insta_bot.find_follower()
